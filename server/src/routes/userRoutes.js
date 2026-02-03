d530875c3fc7c2f9397a9ebfe1e5287d1bf5c2ef
FacuMeoni <meonifacu@gmail.com>
2026-02-03 18:04:58 -0300

    feat: add user routes for admin registration and integrate with main router

---
+++ server/src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { registerAdmin } from "../controllers/userController.js";
+import { tryCatch } from "../utils/tryCatch.js";
+import { authenticateUser, checkUserRole } from "../middlewares/authMiddlewares.js";
+
+const router = Router();
+
+router.post("/register-admin", tryCatch(authenticateUser), tryCatch(checkUserRole("superadmin")), tryCatch(registerAdmin));
+
+
+export default router;

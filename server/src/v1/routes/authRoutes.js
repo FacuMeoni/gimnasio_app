ff0d0636f59694fa7bbd378dc292d7f9c955e917
FacuMeoni <meonifacu@gmail.com>
2026-02-10 11:22:30 -0300

    refactor: update authentication routes to use singular endpoints for superadmin, partner, session, and token management, improving API consistency

--- server/src/v1/routes/authRoutes.js
+++ server/src/v1/routes/authRoutes.js
@@ -9,10 +9,10 @@ import userController from "../controllers/userControllers.js";
 const router = express.Router();
 
 
-router.post("/superadmins", tryCatch(validateUserSchema(schemas.superAdminSchema)), tryCatch(userController.createNewUser));
-router.post("/partners", tryCatch(validateUserSchema(schemas.partnerSchema)), tryCatch(userController.createNewUser));
-router.post("/sessions", tryCatch(authController.createSession));
-router.post("/tokens", tryCatch(refreshLimiter), tryCatch(authController.createNewTokens));
-router.delete("/sessions", tryCatch(authController.revokeSession));
+router.post("/superadmin", tryCatch(validateUserSchema(schemas.superAdminSchema)), tryCatch(userController.createNewUser));
+router.post("/partner", tryCatch(validateUserSchema(schemas.partnerSchema)), tryCatch(userController.createNewUser));
+router.post("/session", tryCatch(authController.createSession));
+router.post("/session/tokens", tryCatch(refreshLimiter), tryCatch(authController.createNewTokens));
+router.delete("/session", tryCatch(authController.revokeSession));
 
 export default router;

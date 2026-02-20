c92c143fa7aff6cde0010e4930af0d0b0bf8ca59
FacuMeoni <meonifacu@gmail.com>
2026-02-20 11:28:42 -0300

    refactor: add underscored option to Sequelize model definition for improved naming conventions

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 import { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } from "../utils/envProvider.js";
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, { logging: false });
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, { logging: false, define: { underscored: true } });
 
 export default sequelize;
